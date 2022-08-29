593bead99e28964b69dabb0e58d78442217a1ac4
Israel Ferreira <48022462+RaelZ@users.noreply.github.com>
2022-08-29 16:35:48 -0300

    heroku config

--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,13 @@
 const Sequelize = require("sequelize")
 require("dotenv").config()
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    dialect: process.env.DB_DIAL,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  }
-)
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+})
 
 module.exports = sequelize
